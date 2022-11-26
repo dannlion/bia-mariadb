55fb4712774b86a5fb136b4c5cd65fb2aa2f810d
dannlion <117786966+dannlion@users.noreply.github.com>
2022-11-26 20:18:27 -0300

    Correction

--- rodar_bia_local.sh
+++ rodar_bia_local.sh
@@ -1,5 +1,1 @@
-docker-compose up -d database
-npm install --loglevel=error
-npm run build --prefix client --loglevel=error
-npx sequelize db:migrate
-npm start
+docker-compose up -d database server
